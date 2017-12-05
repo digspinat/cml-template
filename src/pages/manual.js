1cd779e1fcb17847f5b403f93a8bf3da75851137
Mitesh <cmilan@pistalix.com>
2017-12-05 11:04:07 +0530

    CSS

--- src/pages/manual.js
+++ src/pages/manual.js
@@ -7,11 +7,11 @@ export default ({ data }) => {
       <div style={{ backgroundColor: "#285DAB", padding: "80px"}}>
         <div>
           <p style={{ fontSize: `150px`, color: "white", fontFamily: "Times New Roman" }}>{manual.frontmatter.brand}&nbsp;<img src={manual.frontmatter.logo} style={{ height: `200px`, width: `200px`, verticalAlign: `middle` }} /></p>
-          <p style={{ fontSize: `100px`, fontWeight: `bold`, marginTop: `-30px`, color: "white" }}>{manual.frontmatter.series}</p>
-          <p style={{ fontSize: `100px`, fontWeight: `bold`, marginTop: `-10px`, color: "white" }}>{manual.frontmatter.year}</p>
-          <h1 style={{ fontSize: `80px`, marginTop: `-10px`, color: "#00B900" }}>{manual.frontmatter.manual1}</h1>
-          <h1 style={{ fontSize: `80px`, marginTop: `-10px`, color: "#00B900" }}>{manual.frontmatter.manual2}</h1>
-          <img src={manual.frontmatter.carimage} height="854px" width="900px"/>
+          <p style={{ fontSize: `100px`, fontWeight: `bold`, marginTop: `-100px`, color: "white" }}>{manual.frontmatter.series}</p>
+          <p style={{ fontSize: `100px`, fontWeight: `bold`, marginTop: `-50px`, color: "white" }}>{manual.frontmatter.year}</p>
+          <h1 style={{ fontSize: `80px`, marginTop: `-50px`, color: "#00B900" }}>{manual.frontmatter.manual1}</h1>
+          <h1 style={{ fontSize: `80px`, marginTop: `-50px`, color: "#00B900" }}>{manual.frontmatter.manual2}</h1>
+          <img src={manual.frontmatter.carimage} height="854px" width="850px"/>
           <p style={{ fontSize: `50px`, color: "#CCCCCC", textAlign: `right` }}>{manual.frontmatter.downloadid}</p>
         </div>
       </div>
